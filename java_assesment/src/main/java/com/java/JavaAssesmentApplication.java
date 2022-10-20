8641b7453959a797f981618210debfc0ef81510c
laura.portillo <laura.portillo@solera.com>
2022-10-20 11:39:08 +0200

    first commit

---
+++ java_assesment/src/main/java/com/java/JavaAssesmentApplication.java
@@ -0,0 +1,13 @@
+package com.java;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JavaAssesmentApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JavaAssesmentApplication.class, args);
+	}
+
+}
