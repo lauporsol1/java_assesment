2d50ee532c55f4ec506347500b95974fa19d94cd
Enrique Salazar <57001244+quiquesm@users.noreply.github.com>
2022-10-20 12:20:06 +0200

    Controller avanzao

---
+++ java_assesment/src/main/java/com/java/repositories/CambiarRepository.java
@@ -0,0 +1,12 @@
+package com.java.repositories;
+
+import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
+
+import com.java.models.Cambiar;
+
+@Repository
+public interface CambiarRepository extends CrudRepository<Cambiar, Integer>{
+	
+
+}
---
+++ java_assesment/src/main/java/com/java/services/CambiarServiceImpl.java
@@ -0,0 +1,8 @@
+package com.java.services;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class CambiarServiceImpl {
+
+}
